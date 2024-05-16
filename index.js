1e78fcd81643efc74fcf74248fe15beb803df0cc
nicholas <aigberuan6@gmail.com>
2024-05-16 12:44:37 +0100

    auth

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
 require('dotenv').config();
 require('./db');
-const PORT = 9000;
-
+const PORT = 8000;
 
 app.use(bodyParser.json());
+app.use('/users', userRoutes);
+app.use('/tasks', taskRoutes);
 
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.json({
-        message: 'Task Manager api is all good'
+        message: 'Task Manager API is working!'
     })
 });
-
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}.`);
 });
-
-
-
-
